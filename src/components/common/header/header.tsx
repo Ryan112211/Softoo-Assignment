e913161659763821867fa858ebb3645f54561be4
Rehan Khan <rehan.khan.teo@gotoko.co.id>
2024-03-28 10:36:10 +0500

    feat(navigation): add navigation link

--- src/components/common/header/header.tsx
+++ src/components/common/header/header.tsx
@@ -1,10 +1,13 @@
 import { ShoppingBasketIcon } from 'lucide-react';
 import ModeToggle from '../modeToggle';
+import { Button } from '@/components/ui/button';
 
 const Header = () => {
   return (
     <header className="flex justify-between mx-6 my-2">
-      <ShoppingBasketIcon size={44} />
+      <Button variant="ghost" onClick={() => (window.location.href = '/')}>
+        <ShoppingBasketIcon size={44} />
+      </Button>
       <ModeToggle />
     </header>
   );
